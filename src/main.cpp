b1eae75ac8f8e23763f48cec4d5565f8dc516d66
Hexa <hexa3333@protonmail.com>
2026-06-12 09:56:12 +0300

    EBO's and wireframe mode

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,18 @@ void key_callback(struct GLFWwindow* window, int key, int scancode, int action, 
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
         glfwSetWindowShouldClose(window, GLFW_TRUE);
     }
+
+    // Wireframe mode
+    if (key == GLFW_KEY_P && action == GLFW_PRESS) {
+        static bool wireframe_enabled = false;
+        if (wireframe_enabled) {
+            wireframe_enabled = false;
+            glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+        } else {
+            wireframe_enabled = true;
+            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        }
+    }
 }
 
 int main(void)
@@ -46,11 +58,19 @@ int main(void)
     glfwSetKeyCallback(window, key_callback);
 
 
-    float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-         0.0f,  0.5f, 0.0f
+    GLfloat vertices[] = {
+        // first tri
+        0.5f,  0.5f,  0.0f, // top right
+        0.5f, -0.5f,  0.0f, // bot right
+       -0.5f,  0.5f,  0.0f, // top left
+       -0.5f, -0.5f,  0.0f, // bot left
     };
+
+    GLuint indices[] = {
+        0, 1, 2, // first tri
+        2, 3, 1  // second tri
+    };
+
 
     GLuint vao;
     glGenVertexArrays(1, &vao);
@@ -61,8 +81,15 @@ int main(void)
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    GLuint ebo;
+    glGenBuffers(1, &ebo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
     glEnableVertexAttribArray(0);
+
+    glBindVertexArray(0);
 
     // Read vshader src
     std::ifstream vertex_shader_file_stream("shaders/main.vert");
@@ -131,7 +158,7 @@ int main(void)
 
         glUseProgram(shader_program);
         glBindVertexArray(vao);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
